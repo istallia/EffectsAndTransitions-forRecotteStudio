dabd97ba653aba001edb01181d02d761b6416998
Istallia <55042065+istallia@users.noreply.github.com>
2020-09-11 16:26:31 +0900

    Initial commit (2)

---
+++ ista_effect_wipe-in-circle-1.hlsl
@@ -0,0 +1,24 @@
+#include "util.hlsl"
+
+#define PARAM_DEBUG 0
+
+float4 main( 
+   float4 pos : SV_POSITION, // 正規化されてない
+   float2 uv : UV,
+   float2 uvp : UVP
+   ) : SV_TARGET
+{
+    UnpackedParams p = unpack(
+        Float0, Float1, Float2, Float3, Float4, Float5,
+        Color0, Color1, Color2, Color3);
+
+    #if PARAM_DEBUG
+    float4 dump = dumpParam(p, pos.xy);
+    if(dump.a > 0) return dump;
+    #endif
+
+
+    float4 color = tex(uv);
+    color = nega(color);
+    return ApplyBasicParamater(pos, color);
+}
