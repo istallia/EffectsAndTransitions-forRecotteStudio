67d964cedc51ee186385dd2b635c9192b5e50214
Istallia <55042065+istallia@users.noreply.github.com>
2020-09-11 19:42:59 +0900

    [Add] ワイプアウト(円形/外側)を追加

---
+++ ista_effect_wipe-in-circle-2.hlsl
@@ -0,0 +1,28 @@
+#include "util.hlsl"
+
+#define PARAM_DEBUG 0
+
+float4 main( 
+   float4 pos : SV_POSITION, // 正規化されてない
+   float2 uv : UV,
+   float2 uvp : UVP
+   ) : SV_TARGET
+{
+    float2 objectSize  = float2(Float1, Float2);
+    float effTime      = Float0;
+    float maxRadius    = length(objectSize) / 2;
+    float targetRadius = maxRadius * effTime;
+
+    #if PARAM_DEBUG
+    float4 dump = dumpParam(p, pos.xy);
+    if(dump.a > 0) return dump;
+    #endif
+
+    float4 color = tex(uv);
+    float2 pos2  = (uvp - 0.5) * objectSize;
+    float radius = length(pos2);
+    if(radius > targetRadius) {
+      color.a = 0;
+    }
+    return ApplyBasicParamater(pos, color);
+}
---
+++ ista_effect_wipe-out-circle-1.lua
@@ -0,0 +1,41 @@
+require "effects/lib"
+
+
+-- HACK:
+-- AF_Shaderにすると、VideoObjectにアタッチできなくなる。
+-- これはTransitionにすることで回避できるが、
+-- Transitionはパラメータを露出させることができない。
+-- その為、こちらでShaderの設定をし、TransitionでGetShaderして描画するようにしている。
+function GetInfo()
+	local info = {
+		name = "ISTA_effect_wipe-in-circle-1",
+		displayname = {
+			en = "ISTA_effect_wipe-in-circle-1",
+			ja = "ISTA_ワイプイン(円形/外側)"
+		},
+		tag = "in",
+		-- affects = AF_Shader, 
+		shader = {
+			ps = "../ISTA_effect_wipe-in-circle-1.cso"
+		}
+	}
+	return info
+end
+	
+
+function InitEffect()
+	SetDuration(0.5)
+	AddProperty(NewProperty("curve", { ja="時間曲線", en="TimeCurve"},  "curve"))
+end
+
+
+function ApplyEffect(effInfo, param)
+	local tt     = CurveProp("curve", effInfo.t)
+	local rect   = param.bounds
+	local shader = GetShader("ps")
+	SetShaderFloat(shader, 0, tt)
+	SetShaderFloat(shader, 1, rect.w)
+	SetShaderFloat(shader, 2, rect.h)
+	param.shader = shader
+	return param
+end
