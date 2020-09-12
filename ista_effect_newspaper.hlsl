ce02ce99d73ed1d1e3eff91e1d64129645253477
Istallia <55042065+istallia@users.noreply.github.com>
2020-09-12 15:57:51 +0900

    [Modify] 新聞エフェクトの追加

--- ista_effect_newspaper.hlsl
+++ ista_effect_newspaper.hlsl
@@ -8,16 +8,31 @@
 	float2 uvp : UVP
 ) : SV_TARGET
 {
-	float err_px = Float0;
+	float min_v = Float0;
+	float max_v = Float1;
+	if(max_v < min_v) {
+		float tmp = max_v;
+		max_v = min_v;
+		min_v = tmp;
+	}
+	float color_gain        = 1 / (max_v - min_v);
+	static float4x4 pattern = float4x4(1, 9, 3, 11, 13, 5, 15, 7, 4, 12, 2, 10, 16, 8, 14, 6) / 17;
 
 	#if PARAM_DEBUG
 	float4 dump = dumpParam(p, pos.xy);
 	if(dump.a > 0) return dump;
 	#endif
 
-	static float2 for_err_uv = float2(3.14, 1.414);
-	float2 err_uv            = float2(rand(uv), rand(reflect(uv, for_err_uv)));
-	err_uv                   = err_uv * float2(err_px/pos.x, err_px/pos.y);
-	float4 color             = tex(uv+err_uv);
+	float4 color       = tex(uv);
+	float4 color2      = rgb2Hsv(color);
+	float brightness   = (clamp(color2.z, min_v, max_v) - min_v) * color_gain;
+	float2 pattern_pos = asuint(trunc( fmod(uv*pos.xy, float2(4,4)) ));
+	float threshold    = pattern[pattern_pos.y][pattern_pos.x];
+	if(brightness > threshold) {
+		color = WHITE;
+	} else {
+		color = BLACK;
+	}
+
 	return ApplyBasicParamater(pos, color);
 }
--- ista_effect_newspaper.lua
+++ ista_effect_newspaper.lua
@@ -5,15 +5,15 @@
 -- その為、こちらでShaderの設定をし、TransitionでGetShaderして描画するようにしている。
 function GetInfo()
 	local info = {
-		name = "ISTA_effect_frosted-glass",
+		name = "ISTA_effect_newspaper",
 		displayname = {
-			en = "ISTA_effect_frosted-glass",
-			ja = "ISTA_すりガラス"
+			en = "ISTA_effect_newspaper",
+			ja = "ISTA_新聞"
 		},
 		tag = "video",
 		-- affects = AF_Shader, 
 		shader = {
-			ps = "../ISTA_effect_frosted-glass.cso"
+			ps = "../ISTA_effect_newspaper.cso"
 		}
 	}
 	return info
@@ -22,14 +22,17 @@ end
 
 function InitEffect()
 	SetDuration(0.5)
-	AddProperty(NewProperty("err", { ja="拡散範囲[px]", en="error[px]"},  "int", nil, 10))
+	AddProperty(NewProperty("min_v", { ja="最低の明るさ(0～1)", en="Min Brightness(0～1)"},  "float", nil, 0))
+	AddProperty(NewProperty("max_v", { ja="最高の明るさ(0～1)", en="Max Brightness(0～1)"},  "float", nil, 1))
 end
 
 
 function ApplyEffect(effInfo, param)
-	local err    = GetProperty("err")
+	local min_v  = GetProperty("min_v")
+	local max_v  = GetProperty("max_v")
 	local shader = GetShader("ps")
-	SetShaderFloat(shader, 0, err)
+	SetShaderFloat(shader, 0, min_v)
+	SetShaderFloat(shader, 1, max_v)
 	param.shader = shader
 	return param
 end
