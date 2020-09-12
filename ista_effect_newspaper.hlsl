c3676a1c2e5dee4b062d1bda05668794b3b42a4c
Istallia <55042065+istallia@users.noreply.github.com>
2020-09-12 15:24:12 +0900

    [Add] 新聞エフェクトのファイルの追加

---
+++ ista_effect_newspaper.hlsl
@@ -0,0 +1,23 @@
+#include "util.hlsl"
+
+#define PARAM_DEBUG 0
+
+	float4 main( 
+	float4 pos : SV_POSITION, // 正規化されてない
+	float2 uv  : UV,
+	float2 uvp : UVP
+) : SV_TARGET
+{
+	float err_px = Float0;
+
+	#if PARAM_DEBUG
+	float4 dump = dumpParam(p, pos.xy);
+	if(dump.a > 0) return dump;
+	#endif
+
+	static float2 for_err_uv = float2(3.14, 1.414);
+	float2 err_uv            = float2(rand(uv), rand(reflect(uv, for_err_uv)));
+	err_uv                   = err_uv * float2(err_px/pos.x, err_px/pos.y);
+	float4 color             = tex(uv+err_uv);
+	return ApplyBasicParamater(pos, color);
+}
---
+++ ista_effect_newspaper.lua
@@ -0,0 +1,35 @@
+-- HACK:
+-- AF_Shaderにすると、VideoObjectにアタッチできなくなる。
+-- これはTransitionにすることで回避できるが、
+-- Transitionはパラメータを露出させることができない。
+-- その為、こちらでShaderの設定をし、TransitionでGetShaderして描画するようにしている。
+function GetInfo()
+	local info = {
+		name = "ISTA_effect_frosted-glass",
+		displayname = {
+			en = "ISTA_effect_frosted-glass",
+			ja = "ISTA_すりガラス"
+		},
+		tag = "video",
+		-- affects = AF_Shader, 
+		shader = {
+			ps = "../ISTA_effect_frosted-glass.cso"
+		}
+	}
+	return info
+end
+	
+
+function InitEffect()
+	SetDuration(0.5)
+	AddProperty(NewProperty("err", { ja="拡散範囲[px]", en="error[px]"},  "int", nil, 10))
+end
+
+
+function ApplyEffect(effInfo, param)
+	local err    = GetProperty("err")
+	local shader = GetShader("ps")
+	SetShaderFloat(shader, 0, err)
+	param.shader = shader
+	return param
+end
