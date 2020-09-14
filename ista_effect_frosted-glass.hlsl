a8e1eae6d4eb494584137b1c33642ed7a6408f5c
Istallia <55042065+istallia@users.noreply.github.com>
2020-09-14 13:38:31 +0900

    [Fix] すりガラス エフェクトが動画範囲外のピクセルを参照してしまう問題を修正

--- ista_effect_frosted-glass.hlsl
+++ ista_effect_frosted-glass.hlsl
@@ -18,6 +18,6 @@ float4 main(
   static float2 for_err_uv = float2(3.14, 1.414);
   float2 err_uv            = float2(rand(uv), rand(reflect(uv, for_err_uv))) * 2 - 1;
   err_uv                   = err_uv * float2(err_px/pos.x, err_px/pos.y);
-  float4 color             = tex(uv+err_uv);
+  float4 color             = tex(frac(uv+err_uv+1));
   return ApplyBasicParamater(pos, color);
 }
